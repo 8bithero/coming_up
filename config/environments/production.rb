d10b97924b3e88f8d8b1a71ae47f1be8770bb117
8bit Hero <pkemanes@gmail.com>
2011-12-01 18:19:54 +0200

    Set precompile css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ ComingUp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( pages.js pages.css admin.css )
+  
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
